d006543da35d37781805222530e5878947249b1f
kguen-sun <nguyen.anh.khoa@sun-asterisk.com>
2020-08-05 16:13:43 +0700

    Chapter 12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :load_exists_user, only: %i(show edit destroy)
+  before_action :get_user, only: %i(show edit destroy)
   before_action :logged_in_user, except: %i(show new create)
   before_action :correct_user, only: %i(edit update)
   before_action :admin_user, only: :destroy
@@ -70,9 +70,9 @@ class UsersController < ApplicationController
     redirect_to root_url unless current_user.admin?
   end
 
-  def load_exists_user
+  def get_user
     @user = User.find_by id: params[:id]
-    return if @user.present?
+    return if @user
 
     flash[:danger] = t "users.show.flash_not_found"
     redirect_to root_url
